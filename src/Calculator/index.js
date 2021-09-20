1160c8fedf9f784e4f582867a33aea849144efbd
Robert Adams <adamsiii.robert@gmail.com>
2021-09-20 12:17:34 -0700

    factored out Calculator to separate component

--- src/Calculator/index.js
+++ src/Calculator/index.js
@@ -1,41 +1,9 @@
-import React, { useState } from 'react'
-
-import '../Calculator/Calculator.css'
+import React from 'react'
 
 export default function Calculator() {
+    return (
+        <div>
 
-  const [input, setInput] = useState('');  // start with a string for now
-
-  return (
-    <div>
-
-        <p className='input'>{input}</p>
-        <p className='output'></p>
-
-        <button className='clear-btn' type='button'>C</button>
-
-        <button className='operator-btn' type='button' value='%'>%</button>
-        <button className='operator-btn' type='button' value='÷'>÷</button>
-
-        <button className='numpad-btn' type='button' value='7'>7</button>
-        <button className='numpad-btn' type='button' value='8'>8</button>
-        <button className='numpad-btn' type='button' value='9'>9</button>
-        <button className='operator-btn' type='button' value='×'>×</button>
-
-        <button className='numpad-btn' type='button' value='4'>4</button>
-        <button className='numpad-btn' type='button' value='5'>5</button>
-        <button className='numpad-btn' type='button' value='6'>6</button>
-        <button className='operator-btn' type='button' value='-'>-</button>
-
-        <button className='numpad-btn' type='button' value='1'>1</button>
-        <button className='numpad-btn' type='button' value='2'>2</button>
-        <button className='numpad-btn' type='button' value='3'>3</button>
-        <button className='operator-btn' type='button' value='+'>+</button>
-
-        <button className='numpad-btn' type='button' value='+/-'>+/-</button>
-        <button className='numpad-btn' type='button' value='0'>0</button>
-        <button className='numpad-btn' type='button' value='.'>.</button>
-        <button className='operator-btn' type='button' value='='>=</button>
-    </div>
-  )
+        </div>
+    )
 }
